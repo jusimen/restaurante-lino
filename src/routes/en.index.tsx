b3682ed3249ee01e8e194b2fab408a9e3633e48d
jusimen <jusi.monteiro@protonmail.com>
2026-05-27 23:40:52 +0100

    Remove GallerySection from HomePageEn component to streamline layout.

--- src/routes/en.index.tsx
+++ src/routes/en.index.tsx
@@ -4,7 +4,6 @@ import { Footer } from "@/components/Footer";
 import { HeroSection } from "@/components/HeroSection";
 import { AboutSection } from "@/components/AboutSection";
 import { DishesSection } from "@/components/DishesSection";
-import { GallerySection } from "@/components/GallerySection";
 import { ContactSection } from "@/components/ContactSection";
 
 export const Route = createFileRoute("/en/")({
@@ -44,7 +43,6 @@ function HomePageEn() {
         <HeroSection />
         <AboutSection />
         <DishesSection />
-        <GallerySection />
         <ContactSection />
       </main>
       <Footer />
