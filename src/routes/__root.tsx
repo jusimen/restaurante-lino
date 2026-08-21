d32f6bb12005e786d6db683c450d4a8bd8912d19
jusimen <jusi.monteiro@protonmail.com>
2026-08-21 19:20:32 +0100

    Add Google Tag Manager integration: include script and noscript tags for tracking and analytics support in the RootShell component.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -110,9 +110,31 @@ function RootShell({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-PT">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M9GGLZBV');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <HeadContent />
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M9GGLZBV"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <Scripts />
       </body>
